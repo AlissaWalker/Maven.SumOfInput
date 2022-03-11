d343b4fb9706efd8cadfa929016c30d197523b9f
Alissa <alissa@Zipcoders-MBP-3.hsd1.de.comcast.net>
2022-03-11 01:00:22 -0500

    Lab finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,14 @@
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int n = 100;
+        int sum = 0;
 
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
-}
+
+        }
